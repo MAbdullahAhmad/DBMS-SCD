742501869c2e1f42d90a4351e399f901dc2aeb57
Abdullah Ahmad <theneweratech@gmail.com>
2025-05-31 09:31:58 +0500

    added test home controller

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+class HomeController {
+
+  public function index() {
+    echo "<h1>Welcome to PHP-DBMS</h1>";
+    echo "<p>This is the home page.</p>";
+    echo "<p><a href='/fun'>View Fun Page</a></p>";
+  }
+
+}
