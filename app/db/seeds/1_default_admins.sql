e3e61f1954c04f68a117afa7e99cedbffc5870f8
Abdullah Ahmad <theneweratech@gmail.com>
2025-07-02 19:56:35 +0500

    added seeders and migrations for Admin and Employee

---
+++ app/db/seeds/1_default_admins.sql
@@ -0,0 +1,3 @@
+INSERT INTO sc_admins (username, password_hash) VALUES
+('admin1', '5f4dcc3b5aa765d61d8327deb882cf99'),
+('admin2', '5f4dcc3b5aa765d61d8327deb882cf99');
---
+++ app/db/seeds/2_employees.sql
@@ -0,0 +1,9 @@
+INSERT INTO sc_employees (email, cattr_user_id) VALUES
+('abdullah.faisal@eigensol.com', 1),
+('abdullah@eigensol.com', 2),
+('admin@cattr.app', 3),
+('admin@eigensol.com', 4),
+('arslangondal@eigensol.com', 5),
+('dilawar@eigensol.com', 6),
+('saim@eigensol.com', 7),
+('umer@eigensol.com', 8);
